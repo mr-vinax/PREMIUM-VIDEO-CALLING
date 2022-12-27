f0760d07073278be8c641e55ecfedacfebd8e615
mr-vinax <121479103+mr-vinax@users.noreply.github.com>
2022-12-27 13:12:37 +0530

    Create main.java

---
+++ app/main.java
@@ -0,0 +1,46 @@
+
+//Global Variables
+let users = [];
+
+//Classes
+class User {
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
+        this.room = null;
+    }
+
+    //Methods
+    joinRoom(roomId) {
+        this.room = roomId;
+    }
+}
+
+class Room {
+    constructor(roomId, name) {
+        this.roomId = roomId;
+        this.name = name;
+        this.users = [];
+    }
+
+    //Methods
+    addUser(user) {
+        this.users.push(user);
+    }
+
+    removeUser(userId) {
+        const user = this.getUser(userId);
+
+        if (user) {
+            this.users = this.users.filter(user => user.id !== userId);
+        }
+        return user;
+    }
+
+    getUser(userId) {
+        return this.users.find(user => user.id === userId);
+    }
+}
+
+//Functions
+function createUser(name,
